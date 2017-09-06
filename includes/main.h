168e08c6d1a9b5cbe74b7a6079a71006f7466b55
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2017-09-05 20:10:48 -0400

    Initial commit

---
+++ includes/main.h
@@ -0,0 +1,21 @@
+//
+// Created by jose on 9/5/17.
+//
+
+#ifndef FFNN_MAIN_H
+#define FFNN_MAIN_H
+
+#include <unistd.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <stdint.h>
+#include <limits.h>
+#include <setjmp.h>
+#include <errno.h>
+#include <time.h>
+#include <math.h>
+
+#endif //FFNN_MAIN_H
---
+++ main.c
@@ -0,0 +1,17 @@
+/** This is a basic implementation of a feed-forward backpropagation neural
+ *  network in C, as demonstrated by Dr. John Bullinaria from The University
+ *  of Birmingham Department of Computer Science.
+ *
+ *  @author Dr. John Bullinaria
+ *  @date September 05, 2017
+ *
+ */
+
+#include "includes/main.h"
+
+int main()
+{
+  printf("Hello, World!\n");
+
+  return (EXIT_SUCCESS);
+}
